affe226da117fafd1b7e2d1ad05504c4b0682a26
greenpandastudio <connect@greenpanda.studio>
2025-08-19 15:05:52 +0200

    Added SASS, my base scss files, fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,13 @@
-module.exports = function(eleventyConfig) {
+export default function(eleventyConfig) {
+  eleventyConfig.addWatchTarget("src/_data");
+  eleventyConfig.addWatchTarget("src/scss");
   eleventyConfig.addPassthroughCopy({ "src/assets": "/assets" });
   eleventyConfig.addPassthroughCopy({ "src/admin": "/admin" });
+
   return {
     dir: { input: "src", output: "_site", includes: "_includes", data: "_data" },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     passthroughFileCopy: true
   };
-};
+}
