d5fa06fd65091af20d2f8f9839e5aef319a0386b
greenpandastudio <connect@greenpanda.studio>
2025-09-05 09:54:13 +0200

    Some changes in the Sass. Removed the THEM variable in .env: not useful

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,9 +1,8 @@
-import dotenv from 'dotenv'
 import { execSync } from 'child_process'
+import fs from 'fs'
 
-dotenv.config()
-
-const theme = process.env.THEME || 'default'
+const siteData = JSON.parse(fs.readFileSync('./src/_data/site.json', 'utf-8'))
+const theme = siteData.theme || 'default'
 const command = `sass --watch src/sass:src/assets/css src/themes/${theme}:src/assets/css --style=compressed`
 
 execSync(command, { stdio: 'inherit' })
