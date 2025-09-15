7120f9a829450a12b8b753a443fe7089774cb522
ESDrayton <emiliano.soravia@gmail.com>
2025-09-15 12:55:43 +0200

    chore(dev): dejank

---
+++ packages/@neutrino/core/index.js
@@ -0,0 +1,8 @@
+// ESM entrypoint for the core
+export default function(eleventyConfig) {
+    // example: register a test filter
+    eleventyConfig.addFilter("shout", str => str.toUpperCase());
+  
+    // here you will import your real filters/shortcodes
+  }
+  
